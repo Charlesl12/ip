f4db735f30eeaede86b13d7941e860879e27508d
Charlesl12 <charles-1614@hotmail.com>
2025-01-24 20:42:40 +0800

    rename main class, add greet, add exit

---
+++ src/main/java/Clovis.java
@@ -0,0 +1,7 @@
+public class Clovis {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Clovis");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
