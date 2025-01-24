d0477238c8d15487b276a031de116a43f90f0b62
Charlesl12 <charles-1614@hotmail.com>
2025-01-24 22:16:02 +0800

    add echo

--- src/main/java/Clovis.java
+++ src/main/java/Clovis.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Clovis {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Clovis");
         System.out.println("What can I do for you?\n");
+        while(true) {
+            String input = sc.nextLine();
+            System.out.println(input);
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
