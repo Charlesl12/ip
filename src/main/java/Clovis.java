3a19a422270dce787f9ac9a8e5798a9804ca65e2
Charlesl12 <charles-1614@hotmail.com>
2025-01-24 23:59:04 +0800

    add list

--- src/main/java/Clovis.java
+++ src/main/java/Clovis.java
@@ -1,15 +1,23 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Clovis {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>(100);
         System.out.println("Hello! I'm Clovis");
         System.out.println("What can I do for you?\n");
         while(true) {
             String input = sc.nextLine();
-            System.out.println(input);
-            if (input.equalsIgnoreCase("bye")) {
+            System.out.println("added: " + input);
+            if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i+1) + ". " + list.get(i));
+                }
+            } else if (input.equalsIgnoreCase("bye")) {
                 break;
+            } else {
+                list.add(input);
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
