cfbca7ff56fd88ccbd25655168c42b45bd761126
Charlesl12 <charles-1614@hotmail.com>
2025-02-18 00:27:47 +0800

    Add JavaDocs for Clovis.java, Parser.java, Ui.java, Storage.java, ClovisException.java

--- src/main/java/clovis/ClovisException.java
+++ src/main/java/clovis/ClovisException.java
@@ -1,6 +1,14 @@
 package clovis;
 
+/**
+ * The {@code ClovisException} class represents custom exceptions specific to the Clovis application.
+ */
 public class ClovisException extends Exception {
+    /**
+     * Constructs a new {@code ClovisException} with the specified detail message.
+     *
+     * @param message the detail message describing the exception.
+     */
     public ClovisException(String message) {
         super(message);
     }
--- src/main/java/clovis/Storage.java
+++ src/main/java/clovis/Storage.java
@@ -13,12 +13,27 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.io.IOException;
 
+/**
+ * The {@code Storage} class handles the saving of tasks to a file and retrieval of tasks from a file.
+ */
 public class Storage {
     private final String filePath;
 
+    /**
+     * Constructs a {@code Storage} instance with the specified file path.
+     *
+     * @param filePath the path to the file.
+     */
     public Storage (String filePath) {
         this.filePath = filePath;
     }
+
+    /**
+     * Loads tasks from the file specified by {@code filePath}.
+     *
+     * @return an {@code ArrayList} of tasks loaded from the file.
+     * @throws ClovisException if an error occurs while reading the file.
+     */
     public ArrayList<Task> loadTasks() throws ClovisException {
         ArrayList<Task> tasks = new ArrayList<>();
         File file = new File(filePath);
@@ -54,6 +69,12 @@ public class Storage {
         return tasks;
     }
 
+    /**
+     * Saves tasks into the file specified by {@code filePath}.
+     *
+     * @param tasks takes a list of tasks to be saved.
+     * @throws ClovisException if an error occurs while writing to the file.
+     */
     public void saveTasks(ArrayList<Task> tasks) throws ClovisException {
         try {
             BufferedWriter bw = new BufferedWriter(new FileWriter(filePath));
