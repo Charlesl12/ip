cd6c11eadece6c1ec6f97f227f02068fed01b649
Charlesl12 <charles-1614@hotmail.com>
2025-02-22 22:22:55 +0800

    Add JUnit test for FindCommand.java

---
+++ src/test/java/clovis/command/FindCommandTest.java
@@ -0,0 +1,51 @@
+package clovis.command;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import clovis.task.TaskList;
+import clovis.task.ToDo;
+import clovis.Storage;
+import clovis.Ui;
+import clovis.ClovisException;
+
+
+public class FindCommandTest {
+    private TaskList taskList;
+    private Ui ui;
+    private Storage storage;
+
+    @BeforeEach
+    public void setUp() {
+        taskList = new TaskList(new ArrayList<>());
+        ui = new Ui();
+        storage = new Storage("data/test_tasks.txt");
+        taskList.addTask(new ToDo("Read book"));
+        taskList.addTask(new ToDo("Write code"));
+    }
+
+    @Test
+    public void testExecute_returnsMatchingTasks() throws ClovisException {
+        Command command = new FindCommand("book");
+
+        String result = command.execute(taskList, ui, storage);
+
+        assertTrue(result.contains("Read book"));
+        assertFalse(result.contains("Write code"));
+    }
+
+    @Test
+    public void testExecute_returnsNoMatches() throws ClovisException {
+        Command command = new FindCommand("sleep");
+
+        String result = command.execute(taskList, ui, storage);
+
+        assertEquals("No tasks matching sleep", result);
+    }
+}
--- src/test/java/clovis/task/TaskListTest.java
+++ src/test/java/clovis/task/TaskListTest.java
@@ -1,8 +1,12 @@
 package clovis.task;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 public class TaskListTest {
 
@@ -11,6 +15,7 @@ public class TaskListTest {
         TaskList tasks = new TaskList();
         ToDo todo = new ToDo("read book");
         tasks.addTask(todo);
+
         assertEquals(1, tasks.size());
         assertEquals(todo, tasks.getTask(1));
     }
@@ -23,7 +28,49 @@ public class TaskListTest {
         tasks.addTask(todo1);
         tasks.addTask(todo2);
         Task deletedTask = tasks.deleteTask(1);
+
         assertEquals(todo1, deletedTask);
         assertEquals(1, tasks.size());
+    }
+
+    @Test
+    public void testMarkTask() {
+        TaskList tasks = new TaskList();
+        ToDo todo1 = new ToDo("read book");
+        ToDo todo2 = new ToDo("return book");
+        tasks.addTask(todo1);
+        tasks.addTask(todo2);
+        tasks.markTask(1, true);
+        tasks.markTask(2, false);
+
+        assertEquals(true, todo1.isDone);
+        assertEquals(false, todo2.isDone);
+    }
+
+
+    @Test
+    public void testFindTask_returnsMatchingTask() {
+        TaskList tasks = new TaskList();
+        ToDo todo1 = new ToDo("read book");
+        ToDo todo2 = new ToDo("return book");
+        tasks.addTask(todo1);
+        tasks.addTask(todo2);
+        ArrayList<Task> matchingTasks = tasks.findTask("Read");
+
+        assertEquals(1, matchingTasks.size());
+        assertEquals(todo1, matchingTasks.get(0));
+    }
+
+    @Test
+    public void testFindTask_returnsNoMatchingTask() {
+        TaskList tasks = new TaskList();
+        ToDo todo1 = new ToDo("read book");
+        ToDo todo2 = new ToDo("return book");
+        tasks.addTask(todo1);
+        tasks.addTask(todo2);
+        ArrayList<Task> matchingTasks = tasks.findTask("sLeep");
+
+        assertEquals(1, matchingTasks.size());
+        assertTrue(matchingTasks.isEmpty());
     }
 }
